5ec375c5ffe24a8dcf77644bd950908be7596e48
Dheeshana De Silva <dheeshanadesilva2002@gmail.com>
2026-05-28 01:30:36 +0530

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
 
         role: {
             type: String,
-            enum: ["USER", "ADMIN"],
+            enum: ["USER", "SELLER", "ADMIN"],
             default: "USER",
         },
 
